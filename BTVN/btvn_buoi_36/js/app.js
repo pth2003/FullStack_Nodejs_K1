562bbee78961028503555e8de6aa9898742643dd
Phan Trung  Hiếu <phantrunghieu1412@gmail.com>
2023-10-11 00:35:57 +0700

    36

--- BTVN/btvn_buoi_36/js/app.js
+++ BTVN/btvn_buoi_36/js/app.js
@@ -37,40 +37,47 @@ const question = document.querySelector("#question");
 const options = document.querySelector(".quiz-options");
 const _corretScore = document.querySelector("#correct-score");
 const _totalQuestion = document.querySelector("#total-question");
-
+const result = document.querySelector("#result");
 const checkBtn = document.querySelector("#check-answer");
-console.log(checkBtn);
+const playAgain = document.querySelector("#play-again");
 
 let correctAnswer = "",
-  corretScore = 0,
   askedCount = 0,
   totalQuestion = 10;
-
+let corretScore = 0;
+console.log(corretScore);
+let eventListener = () => {
+  checkBtn.addEventListener("click", checkAnswer);
+  playAgain.addEventListener("click", restartGame);
+};
 document.addEventListener("DOMContentLoaded", () => {
   getData();
+  eventListener();
   _totalQuestion.textContent = totalQuestion;
   _corretScore.textContent = corretScore;
+  console.log(corretScore);
 });
-console.log(options);
-const render = (data) => {
-  correctAnswer = data.correct_answer;
 
+const render = (data) => {
+  checkBtn.disabled = false;
+  // result.innerHTML = "";
+  correctAnswer = data.correct_answer;
   let incorrectAnswer = data.incorrect_answers;
-  console.log(incorrectAnswer);
+
   let optionList = incorrectAnswer;
   optionList.splice(
     Math.floor(Math.random() * (incorrectAnswer.length + 1)),
     0,
     correctAnswer
   );
-  console.log(optionList);
+
   question.innerHTML = `${data.question} <br />
   <span class="category">${data.category}</span>`;
 
   options.innerHTML = `
     ${optionList
       .map((option, index) => {
-        return `<li> ${index + 1}. <span> ${option}</span> </li>`;
+        return `<li> ${index + 1}. <span>${option}</span> </li>`;
       })
       .join("")}
   `;
@@ -79,10 +86,12 @@ const getData = async () => {
   const res = await fetch(api);
   const data = await res.json();
   render(data.results[0]);
+  result.innerHTML = "";
   selectOption();
 };
 
 const selectOption = () => {
+  console.log(correctAnswer);
   options.querySelectorAll("li").forEach((option) => {
     option.addEventListener("click", () => {
       if (options.querySelector(".selected")) {
@@ -94,4 +103,49 @@ const selectOption = () => {
   });
 };
 
-checkBtn.addEventListener("click", () => {});
+const checkAnswer = () => {
+  checkBtn.disabled = true;
+  if (options.querySelector(".selected")) {
+    let selectAnswer = options.querySelector(".selected span").innerText;
+
+    if (selectAnswer === correctAnswer) {
+      corretScore++;
+
+      console.log(corretScore);
+      result.innerHTML = `<p> <i class="fa-solid fa-check"></i>Correct Answer! </p>`;
+    } else {
+      result.innerHTML = `<p> <i class="fa-solid fa-xmark"></i>Incorrect Answer! </p> 
+        <p><small><b>Correct Answer : </b>${correctAnswer}</small></p>
+      `;
+    }
+    checkCount();
+  }
+};
+
+const checkCount = () => {
+  askedCount++;
+  setCount();
+  if (askedCount === totalQuestion) {
+    result.innerHTML = `<p> Your score is : ${corretScore}. </p>`;
+    playAgain.style.display = "block";
+    checkBtn.style.display = "none";
+  } else {
+    setTimeout(() => {
+      getData();
+    }, 300);
+  }
+};
+const setCount = () => {
+  _totalQuestion.textContent = totalQuestion;
+  _corretScore.textContent = corretScore;
+};
+
+const restartGame = () => {
+  corretScore = 0;
+  askedCount = 0;
+  playAgain.style.display = "none";
+  checkBtn.style.display = "block";
+  checkBtn.disabled = false;
+  setCount();
+  getData();
+};
